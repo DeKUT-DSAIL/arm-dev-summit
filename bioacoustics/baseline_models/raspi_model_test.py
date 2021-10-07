cf1f54dfff951c7568e9562b8f536d70ca71e03e
dsail dekut <dsail@jetson.local>
2021-10-07 17:52:50 +0300

    modified streaming parameters

--- bioacoustics/baseline_models/raspi_model_test.py
+++ bioacoustics/baseline_models/raspi_model_test.py
@@ -1,10 +1,7 @@
 import os
-import busio
-import board
 import pickle
 import librosa
 import argparse
-import digitalio
 import numpy as np
 import pandas as pd
 import configparser
@@ -14,6 +11,8 @@ import sounddevice as sd
 from collections import Counter
 import features_generation as fg
 import preprocessing_functions as pf
+import Jetson.GPIO as GPIO
+
 
 parser = argparse.ArgumentParser(description='Test trained models')
 parser.add_argument('-m',
@@ -62,25 +61,14 @@ parser.add_argument('-a',
 args = parser.parse_args()
 
 
-
 q = Queue()
 
-#std_dev = 0.0004
-#mean = 0.076
+
 df_annotation = pd.read_csv(args.annotation_csv_path)
 species = list(set(df_annotation['label']))
 species.sort()
 
-
-grey_backed_camaroptera = digitalio.DigitalInOut(board.D18) #Set the GPIO PIN 18 as a digital pin
-grey_backed_camaroptera.direction = digitalio.Direction.OUTPUT #Set GPIO PIN 18 as an output pin
-
-hartlaubs_turaco = digitalio.DigitalInOut(board.D23) #Set the GPIO PIN 18 as a digital pin
-hartlaubs_turaco.direction = digitalio.Direction.OUTPUT #Set GPIO PIN 18 as an output pin
-
-tropical_boubou = digitalio.DigitalInOut(board.D24) #Set the GPIO PIN 18 as a digital pin
-tropical_boubou.direction = digitalio.Direction.OUTPUT #Set GPIO PIN 18 as an output pin
-
+GPIO.setwarnings(False)
 
 
 def audio_callback(indata, frames, time, status):
@@ -102,15 +90,9 @@ def calibration(num_calibration_blocks):
             block = block.flatten()
             energy = np.sum(block ** 2)
             l.append(energy)
-        std_dev = np.std(l[200:])
-        mean = np.mean(l[200:])
-#         l = []
-#         l.extend((date, mean, std_dev))
+        std_dev = np.std(l)
+        mean = np.mean(l)
 
-#         with open('calibration.csv', mode = 'a') as file:
-#             create = csv.writer(file)
-#             create.writerow(l)
-#        print(mean, std_dev)
         print('Done Calibrating')
         return mean, std_dev
 
@@ -125,27 +107,29 @@ def block_energy():
     return energy, my_block
 
 
-def species_gpio(predicted_species):
+def pin_blink(pin):
     """Drives different GPIO pins high to light up leds to indicate the
-    detected species"""
+       detected species
+       Args: pin- pin number (board mode)
+    """
 
-    if predicted_species == 'grey-backed camaroptera':
-        grey_backed_camaroptera.value = True
-        sleep(5)
-        grey_backed_camaroptera.value = False
 
-    elif predicted_species == 'hartlaub\'s turaco':
-        hartlaubs_turaco.value = True
-        sleep(5)
-        hartlaubs_turaco.value = False
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
 
-    elif predicted_species == 'tropical boubou':
-        tropical_boubou.value = True
-        sleep(5)
-        tropical_boubou.value = False
+    try:
+        GPIO.output(pin, GPIO.HIGH)
+        sleep(2)
+        GPIO.output(pin, GPIO.LOW)
+
+    except KeyboardInterrupt:
+        GPIO.output(pin, GPIO.LOW)
+
+
+
+
 def main():
-    """Returns an array of audio samples
-    Args: audio_file- True if a saved audio will be used False if a streamed audio will be used
+    """load and tests trained model
     """
 
     # Get parameters from configuration file
@@ -154,21 +138,28 @@ def main():
 
     win_len_ms = int(config['audio']['win_len_ms'])
     overlap = float(config['audio']['overlap'])
-    stream_len = float(config['audio']['stream_len'])
-    channels = int(config['audio']['channels'])
     sampling_rate = int(config['audio']['sampling_rate'])
-    calibration_duration = int(config['audio']['calibration_duration'])
+    duration = float(config['audio']['duration'])
+    num_mels = float(config['audio']['num_mels'])
 
-    duration = float(config['baseline']['duration'])
-    num_mels = float(config['baseline']['num_mels'])
+    calibration_len_s = int(config['stream']['calibration_len_s']) #length of stream in secs to be used to calibrate the system
+    block_len_ms = int(config['stream']['block_len_ms']) #block length to process for activity detection
+    stream_len_s = int(config['stream']['stream_len_s']) #len in seconds to stream for prediction
+    stream_samplerate = int(config['stream']['stream_samplerate']) #sampling rate of the stream
+    channels = int(config['stream']['channels'])
+
+
 
     # Derive audio processing values
-    win_len = int((win_len_ms * sampling_rate) / 1000)
+    win_len = int((win_len_ms * sampling_rate) / 1000) #window size to perform FFT on
     hop_len = int(win_len * (1 - overlap))
     nfft = int(2 ** np.ceil(np.log2(win_len)))
     num_frame = int((0.5 * duration * sampling_rate) / hop_len)
-    stream_frames = int((sampling_rate / win_len) * stream_len) #number of blocks to save as a single file
-    num_calibration_blocks = int(calibration_duration / (win_len_ms / 1e3)) #number of blocks to set the still condition
+
+    #derive streaming values
+    block_len = int((block_len_ms * stream_samplerate) / 1000) #blocksize to process for activity detection
+    stream_frames = int((stream_samplerate / block_len) * stream_len_s) #number of blocks to process for prediction
+    num_calibration_blocks = int(calibration_len_s / (block_len_ms / 1e3)) #number of blocks to set the still condition
 
     if args.model == 'svm':
         clf = pickle.load(open(os.path.join(args.models_dir, args.model + '.pickle'), 'rb'))
@@ -208,9 +199,10 @@ def main():
 
     else:
         try:
-            with sd.InputStream(samplerate = sampling_rate,
-                                blocksize = win_len,
-                                channels = args.channels,
+            with sd.InputStream(samplerate = stream_samplerate,
+                                blocksize = block_len,
+                                device=11,
+                                channels = channels,
                                 callback = audio_callback):
 
                 mean, std_dev = calibration(num_calibration_blocks)
@@ -226,6 +218,11 @@ def main():
                             my_block = q.get()
                             my_block = my_block.flatten()
                             audio = np.concatenate((audio, my_block))
+                        print(audio.shape)
+
+                        audio = librosa.resample(audio, stream_samplerate, sampling_rate)
+
+                        print(audio.shape)
 
                         feature = fg.features_extraction(audio,
                                                      nfft,
@@ -236,50 +233,30 @@ def main():
                                                      win_len,
                                                      hop_len,
                                                      num_mels)
-                        X = pf.all_summary_features(feature, num_frame)
-                        #print(X.shape)
-                        if X.size:
-                            #print('predicting')
-                            predicted = clf.predict(X)
-                            predictions = [species[int(i)] for i in predicted]
-                            if len(predictions) == 1:
-
-                                predicted_species = predictions[0]
-                                print('predicted species is: ', predicted_species)
-                                species_gpio(predicted_species)
-
-                            elif len(predictions) == 2:
-                                if predictions[0] == predictions[1]:
-                                    predicted_species = predictions[0]
-                                    print('predicted species is: ', predicted_species)
-                                    species_gpio(predicted_species)
-
-                                else:
-                                    print('Unclear')
-
-                            elif len(predictions) >= 3:
-                                num_birds = Counter(predictions)
-                                most_frequent = num_birds.most_common(4)
-
-                                if len(most_frequent) == 1:
-                                    predicted_species = predictions[0]
-                                    print('predicted species is: ', predicted_species)
-                                    species_gpio(predicted_species)
 
 
-                                else:
-                                    # counts = [count[1] for count in most_frequent]
-                                    if most_frequent[0][1] > most_frequent[1][1]:
-                                        predicted_species = most_frequent[0][0]
-                                        print('predicted species is: ', predicted_species)
-                                        species_gpio(predicted_species)
+                        feature = np.log(feature + 1e-8)
 
-                                    else:
-                                        print('Unclear')
+                        features = np.concatenate((np.mean(feature, axis=1),
+                                                            np.std(feature, axis=1))).reshape(1, -1)
 
-                        else:
-                            print('Inaudible')
 
+
+                        print('predicting')
+                        predicted = clf.predict(features)
+                        predicted = species[int(predicted[:])]
+                        print(predicted)
+
+                        if predicted == 'grey-backed camaroptera':
+                            pin = 19
+
+                        elif predicted == 'hartlaub\'s turaco':
+                            pin = 21
+
+                        elif predicted == 'tropical boubou':
+                            pin = 23
+
+                        pin_blink(pin)
 
 
         except Exception as e:
